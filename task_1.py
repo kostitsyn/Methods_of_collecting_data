25f749ec41b0f0d9f0eb51549a8fe4e358ae2d67
kostitsyn <kostitsin.a@mail.ru>
2021-08-03 20:18:12 +0400

    Первое домашнее задание

--- task_1.py
+++ task_1.py
@@ -1,34 +1,19 @@
 import requests
 import json
-import os
-token = os.environ.get("GITHUB_TOKEN")
 
-# url = 'https://api.github.com/users/repos'
-# url = 'https://api.github.com/user'
-# url = 'https://api.github.com/events'
 url = 'https://api.github.com/users/kostitsyn/repos'
-# url = 'https://api.github.com/user/repos'
 
-spam = 'application/vnd.github.v3+json'
+accept_header = 'application/vnd.github.v3+json'
 
 headers = {
-    'Accept': spam,
-}
-
-username = 'kostitsyn'
-password = '6YUJDKlose3830overLOAD'
-
-params = {
-    username: password
+    'Accept': accept_header,
 }
 
 response = requests.get(url, headers=headers)
-# print(response.status_code)
-# print(response.text)
 j_data = response.json()
+
 #  Выводим список репозиториев для конкретного пользователя
 print(f'Список репозиториев пользователя {j_data[1].get("owner").get("login")}:\n')
-# print(json.dumps(j_data, indent=4))
 for i in j_data:
     print(i['name'])
 
