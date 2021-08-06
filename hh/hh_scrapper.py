6ed0bd5a6e2d01704610b08ff353e7a80f2488ec
kostitsyn <kostitsin.a@mail.ru>
2021-08-06 13:07:44 +0400

    Второе домашнее задание

--- hh/hh_scrapper.py
+++ hh/hh_scrapper.py
@@ -5,6 +5,8 @@ import re
 
 
 class HhScrapper:
+    """Скраппер ресурса hh.ru"""
+
     def __init__(self, url, search_part_url, headers, params):
         self.url = url
         self.search_part_url = search_part_url
@@ -13,30 +15,42 @@ class HhScrapper:
         self.result_list = []
 
     def run(self):
+        """Запуск работы скраппера."""
+
         self.create_response_obj()
         self.create_parser_obj()
         self.fill_result_list()
 
     def create_response_obj(self):
+        """Создать объект Response метода GET библиотеки requests."""
+
         self.response = requests.get(f'{self.url}{self.search_part_url}',
                                      params=self.params, headers=self.headers)
 
     def create_parser_obj(self):
+        """Создать объект класса BeautifulSoup."""
+
         self.soup = bs(self.response.text, 'html.parser')
 
     def get_entities_list(self):
+        """Получить список с сущностями."""
+
         entities_list = self.soup.findAll('div', {'class': 'vacancy-serp-item'})
         return entities_list
 
     def fill_result_list(self):
+        """Заполнить результирующий список с данными вакансий."""
+
         entities_list = self.get_entities_list()
         for entity in entities_list:
-            entity_data_dict = self.create_entity_dict(entity)
+            entity_data_dict = self.create_vacancy_dict(entity)
             self.result_list.append(entity_data_dict)
 
         self.change_page()
 
     def change_page(self):
+        """Сменить страницу с вакансиями."""
+
         try:
             update_part_url = self.soup.find('a', {'data-qa': 'pager-next'})['href']
         except TypeError:
@@ -46,7 +60,9 @@ class HhScrapper:
             self.create_parser_obj()
             self.fill_result_list()
 
-    def create_entity_dict(self, entity):
+    def create_vacancy_dict(self, entity):
+        """Создать словарь с данными текущей вакансии."""
+
         entity_dict = dict()
 
         vacancy_name = self.get_vacancy_name(entity)
@@ -64,10 +80,14 @@ class HhScrapper:
         return entity_dict
 
     def get_vacancy_name(self, entity):
+        """Получить название вакансии."""
+
         vacancy_name = entity.find('a', {'class': 'bloko-link'}).text
         return vacancy_name
 
     def get_salary(self, entity):
+        """Получить данные о зарплате: минимальная, максимальная, в какой валюте."""
+
         salary_elem = entity.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'})
         if salary_elem:
             salary_string = entity.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).text
@@ -97,28 +117,39 @@ class HhScrapper:
         return salary_data_dict
 
     def get_link_of_vacancy(self, entity):
+        """Получить ссылку на вакансию."""
+
         link_of_vacancy = entity.find('a', {'class': 'bloko-link'})['href']
         return link_of_vacancy
 
     def get_site_source(self):
+        """Получить источник ресурса из которого взята вакансия."""
+
         site_source = self.url.split('//')[1]
         return site_source
 
     def write_json(self):
-        with open('data.json', 'w') as f:
+        """Записать результирующий список с данными вакансий в json файл."""
+
+        with open('hh/hh_data.json', 'w') as f:
             json.dump(self.result_list, f, sort_keys=True, indent=4, ensure_ascii=False)
 
 
-URL = 'https://hh.ru'
+# Для отладки скраппера.
+if __name__ == '__main__':
 
-SEARCH_PART_URL = '/search/vacancy'
+    hh_url = 'https://hh.ru'
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
-                         ' (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+    hh_search_part_url = '/search/vacancy'
 
-params = {
+    hh_params = {
     'area': '1',
     'fromSearchLine': 'true',
     'st': 'searchVacancy',
     'text': 'python',
-}
+    }
+
+    HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
+              ' (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+    sj_obj = HhScrapper(hh_url, hh_search_part_url, HEADERS, hh_params)
+    sj_obj.run()
--- sj/superjob_scrapper.py
+++ sj/superjob_scrapper.py
@@ -5,6 +5,8 @@ import re
 
 
 class SuperjobScrapper:
+    """Скраппер ресурса superjob.ru"""
+
     def __init__(self, url, search_part_url, headers, params):
         self.url = url
         self.search_part_url = search_part_url
@@ -13,30 +15,42 @@ class SuperjobScrapper:
         self.result_list = []
 
     def run(self):
+        """Запуск работы скраппера."""
+
         self.create_response_obj()
         self.create_parser_obj()
         self.fill_result_list()
 
     def create_response_obj(self):
+        """Создать объект Response метода GET библиотеки requests."""
+
         self.response = requests.get(f'{self.url}{self.search_part_url}',
                                      params=self.params, headers=self.headers)
 
     def create_parser_obj(self):
+        """Создать объект класса BeautifulSoup."""
+
         self.soup = bs(self.response.text, 'html.parser')
 
     def get_entities_list(self):
-        entities_list = self.soup.findAll('div', {'class': ['Fo44F', 'QiY08', 'LvoDO']})
+        """Получить список с сущностями."""
+
+        entities_list = self.soup.findAll('div', {'class': 'f-test-vacancy-item'})
         return entities_list
 
     def fill_result_list(self):
+        """Заполнить результирующий список с данными вакансий."""
+
         entities_list = self.get_entities_list()
         for entity in entities_list:
-            entity_data_dict = self.create_entity_dict(entity)
+            entity_data_dict = self.create_vacancy_dict(entity)
             self.result_list.append(entity_data_dict)
 
         self.change_page()
 
     def change_page(self):
+        """Сменить страницу с вакансиями."""
+
         try:
             update_part_url = self.soup.find('a', {'class': 'f-test-button-dalshe'})['href']
         except TypeError:
@@ -46,7 +60,9 @@ class SuperjobScrapper:
             self.create_parser_obj()
             self.fill_result_list()
 
-    def create_entity_dict(self, entity):
+    def create_vacancy_dict(self, entity):
+        """Создать словарь с данными текущей вакансии."""
+
         entity_dict = dict()
 
         vacancy_name = self.get_vacancy_name(entity)
@@ -64,14 +80,18 @@ class SuperjobScrapper:
         return entity_dict
 
     def get_vacancy_name(self, entity):
+        """Получить название вакансии."""
+
         vacancy_name = entity.find('a').text
         return vacancy_name
 
     def get_salary(self, entity):
+        """Получить данные о зарплате: минимальная, максимальная, в какой валюте."""
+
         salary_string = entity.find('span', {'class': 'f-test-text-company-item-salary'}).text
         by_agreement = re.match('\D+\s\D+', salary_string)
         if not by_agreement:
-            is_interval = re.match('.+ — .+', salary_string)
+            is_interval = re.match('.+\s—\s.*', salary_string)
             if is_interval:
                 min_salary = int(''.join(re.findall('(^\d+)\s(\d*)', salary_string)[0]))
                 max_salary = int(''.join(re.findall('—\s(\d+)\s(\d*)', salary_string)[0]))
@@ -82,7 +102,7 @@ class SuperjobScrapper:
                     min_salary = int(''.join(re.findall('^(\d+)\s(\d*)', salary_string)[0]))
                     max_salary = min_salary
                     currency = re.findall('\s(\D+\.)', salary_string)[0]
-                if str(salary_string).startswith('о'):
+                elif str(salary_string).startswith('о'):
                     min_salary = int(''.join(re.findall('\s(\d+)\s(\d*)', salary_string)[0]))
                     max_salary = None
                     currency = re.findall('\s(\D+\.)', salary_string)[0]
@@ -102,21 +122,26 @@ class SuperjobScrapper:
         return salary_data_dict
 
     def get_link_of_vacancy(self, entity):
+        """Получить ссылку на вакансию."""
+
         link_of_vacancy = entity.find('a')['href']
         return f'{self.url}{link_of_vacancy}'
 
     def get_site_source(self):
+        """Получить источник ресурса из которого взята вакансия."""
+
         site_source = self.url.split('//')[1]
         return site_source
 
     def write_json(self):
-        with open('data1.json', 'w') as f:
+        """Записать результирующий список с данными вакансий в json файл."""
+
+        with open('sj/sj_data.json', 'w') as f:
             json.dump(self.result_list, f, sort_keys=True, indent=4, ensure_ascii=False)
 
 
+# Для отладки скраппера.
 if __name__ == '__main__':
-    # profession = input('Введите интересующую профессию: ')
-    # params['text'] = profession
 
     sj_url = 'https://www.superjob.ru'
 
@@ -127,6 +152,6 @@ if __name__ == '__main__':
         'geo%5Bt%5D%5B0%5D': '4'
     }
     HEADERS = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36'
-                             ' (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+              ' (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
     sj_obj = SuperjobScrapper(sj_url, sj_search_part_url, HEADERS, sj_params)
     sj_obj.run()
