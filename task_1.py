7458403ebf82743c3e8110a19cc1bc69dfe58aaa
kostitsyn <kostitsin.a@mail.ru>
2021-08-17 20:46:45 +0400

    Пятое домашнее задание

--- task_1.py
+++ task_1.py
@@ -1,1 +1,46 @@
-print('ipsum' in 'lorem ipsum dolores')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
+
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("start-maximized")
+
+driver = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_options)
+
+driver.get("https://e.mail.ru/inbox/?back=1")
+
+
+wait = WebDriverWait(driver, 3)
+account_field = wait.until(EC.presence_of_element_located((By.NAME, 'username')))
+
+account_field.send_keys('study.ai_172@mail.ru')
+account_field.submit()
+
+password_field = wait.until(EC.element_to_be_clickable((By.NAME, 'password')))
+
+password_field.send_keys('NextPassword172!!!')
+password_field.submit()
+
+links_list = list()
+
+while True:
+    try:
+        letters_link = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@class, 'llc')]")))
+        actions = ActionChains(driver)
+
+        for i in letters_link:
+            links_list.append(i.get_attribute('href'))
+
+        actions.move_to_element(letters_link[-1])
+        actions.perform()
+    except TimeoutException:
+        break
+print()
--- task_2.py
+++ task_2.py
@@ -1,37 +1,43 @@
+import json
+import pandas as pd
+from pymongo import MongoClient
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
-import json
-from pprint import pprint
-import pandas as pd
-from pymongo import MongoClient
 
 
 class MVideoScrapper:
-    def __init__(self, section_name):
+    """Скраппер новых товаров в интернет-магазине МВидео."""
+
+    def __init__(self):
         self.url = 'https://www.mvideo.ru/?cityId=CityCZ_6273'
-        self.section_name = section_name
 
     def create_chrome_driver(self):
+        """Создать драйвер библиотеки Selenium для браузера Chrome."""
+
         chrome_options = Options()
         chrome_options.add_argument('start-maximized')
         self.driver = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_options)
         self.move_to_url()
 
     def move_to_url(self):
+        """Отправить get-запрос на указанный url."""
+
         self.driver.get(self.url)
         self.close_modal_window()
 
     def close_modal_window(self):
+        """Закрыть модальное окно всплывающее при открытии страницы."""
+
         close_btn = self.driver.find_element_by_xpath("//span[@data-close]")
         close_btn.click()
         self.move_to_goods_list()
 
     def move_to_goods_list(self):
+        """Перейти (пролистать вниз) к разделу новых товаров."""
+
         new_goods = self.driver.find_element_by_xpath(f"//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']")
         actions = ActionChains(self.driver)
         actions.move_to_element(new_goods)
@@ -39,6 +45,8 @@ class MVideoScrapper:
         self.upload_all_goods()
 
     def upload_all_goods(self):
+        """Загрузить все элементы новых товаров путем нажатия на стрелку промотки вправо."""
+
         while True:
             wait = WebDriverWait(self.driver, 5)
             try:
@@ -50,11 +58,15 @@ class MVideoScrapper:
             next_btn.click()
 
     def get_all_goods_elem(self):
+        """Получить все элементы новых товаров."""
+
         elems = self.driver.find_elements_by_xpath(
             f"//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']//li//a[@data-product-info and contains(@class, 'tile-picture')]")
         return elems
 
     def get_data_list(self):
+        """Получить список данных о товарах."""
+
         self.create_chrome_driver()
 
         data_list = list()
@@ -64,23 +76,26 @@ class MVideoScrapper:
             res_dict['price'], res_dict['product_name'], res_dict['product_category'] = \
                 data_dict['productPriceLocal'], data_dict['productName'], data_dict['productCategoryName']
             data_list.append(res_dict)
-        list_for_write = data_list.copy()
-        self.write_in_db(list_for_write)
         return data_list
 
     def create_db_cursor(self):
+        """Создать курсор для записи в БД."""
+
         client = MongoClient('127.0.0.1', 27017)
         db = client['database']
-        self.db_cursor = db.mvideo_goods
+        db_cursor = db.mvideo_goods
+        return db_cursor
 
     def write_in_db(self, data_list):
-        self.create_db_cursor()
-        self.db_cursor.delete_many({})
-        self.db_cursor.insert_many(data_list)
+        """Записать данные в БД."""
+
+        db_cursor = self.create_db_cursor()
+        db_cursor.delete_many({})
+        db_cursor.insert_many(data_list)
 
 
 if __name__ == '__main__':
-    scrapper_obj = MVideoScrapper('Новинки')
+    scrapper_obj = MVideoScrapper()
     res_list = scrapper_obj.get_data_list()
 
     pd.set_option('display.max_rows', None)
@@ -88,45 +103,4 @@ if __name__ == '__main__':
     pd.set_option('display.width', None)
     pd.set_option('display.max_colwidth', None)
 
-    pprint(pd.DataFrame(res_list))
-
-
-# chrome_options = Options()
-# chrome_options.add_argument('start-maximized')
-#
-# driver = webdriver.Chrome(executable_path='./chromedriver_linux64/chromedriver', options=chrome_options)
-#
-# driver.get('https://www.mvideo.ru/?cityId=CityCZ_6273')
-#
-# close_btn = driver.find_element_by_xpath("//span[@data-close]")
-# close_btn.click()
-#
-# new_goods = driver.find_element_by_xpath("//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']")
-# actions = ActionChains(driver)
-# actions.move_to_element(new_goods)
-# actions.perform()
-#
-# while True:
-#     wait = WebDriverWait(driver, 5)
-#     # next_btn = wait.until(EC.element_located_selection_state_to_be((By.XPATH, "//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']//a[contains(@class, 'next-btn')]")))
-#     try:
-#         next_btn = driver.find_element_by_xpath("//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']//a[contains(@class, 'next-btn')]")
-#         spam = next_btn.get_attribute('class')
-#         wait.until(lambda x: 'disabled' not in next_btn.get_attribute('class'))
-#     except TimeoutException:
-#         break
-#
-#     next_btn.click()
-#
-#
-# elems = driver.find_elements_by_xpath("//h2[contains(text(), 'Новинки')]/ancestor::div[@class='section']//li//a[@data-product-info and contains(@class, 'tile-picture')]")
-#
-# res_list = []
-# for i in elems:
-#     res_dict = dict()
-#     data_dict = json.loads(i.get_attribute('data-product-info'))
-#     res_dict['price'], res_dict['product_name'], res_dict['product_category'] = \
-#         data_dict['productPriceLocal'], data_dict['productName'], data_dict['productCategoryName']
-#     res_list.append(res_dict)
-
-
+    print(pd.DataFrame(res_list))
